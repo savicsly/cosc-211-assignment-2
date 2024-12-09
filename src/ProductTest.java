1e5d197dcb5df9e7b67ac7e4406a87c6c5f556ca
Victor Bala <savicsly@gmail.com>
2024-12-09 10:52:50 +0100

    refactor: update ProductTest to include price reduction output and remove TestProduct

--- src/ProductTest.java
+++ src/ProductTest.java
@@ -3,12 +3,14 @@ public class ProductTest {
         Product product1 = new Product("Product1", 20.0);
         Product product2 = new Product("Product2", 30.0);
 
+        System.out.println("Before price reduction:");
         product1.printProduct();
         product2.printProduct();
 
         product1.setPrice(product1.getPrice() - 5);
         product2.setPrice(product2.getPrice() - 5);
 
+        System.out.println("After price reduction:");
         product1.printProduct();
         product2.printProduct();
     }
